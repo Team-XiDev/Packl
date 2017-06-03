c4a130fb22a0c885cbdda39f111dcc7e0c80b0d5
Team-XiDev <team.xidev@gmail.com>
2017-06-03 18:11:54 +0200

    Update menu.php

--- include/menu.php
+++ include/menu.php
@@ -1,1 +1,26 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="css/menu.css" rel="stylesheet">
+        <script type="text/javascript" src="menu.js"></script>
+    </head>
+    <body>
+        <h1>Packl</h1>
+        <ul id="menu">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Pack</a>
+                <ul>
+                    <li><a href="#">Liste de packs</a></li>
+                    <li><a href="#">Pack Maker</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Membre</a>
+                <ul>
+                    <li><a href="#">Connexion</a></li>
+                    <li><a href="#">Inscription</a></li>
+                </ul>
+            </li>
+        </ul>
+</body>
+</html>
